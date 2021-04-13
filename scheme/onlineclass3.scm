f60369ad0d9730f95f1a5c2b9faecd298dc989f8
Wade Huber <wadehuber@gmail.com>
2021-04-12 21:20:27 -0700

    Spring 2021 updates.

--- scheme/onlineclass3.scm
+++ scheme/onlineclass3.scm
@@ -1,5 +1,4 @@
 #lang scheme
-
 "Scheme Review"
 
 (define x 5)
@@ -39,10 +38,10 @@
 (newline)
 "Example list procedures"
 (define add-list
-  (lambda (lst)
-    (if (null? lst)
+  (lambda (x)
+    (if (null? x)
         0
-        (+ (car lst) (add-list (cdr lst))))))
+        (+ (car x) (add-list (cdr x))))))
 (add-list '(10 37 39))
 (add-list lst)
 
@@ -61,29 +60,34 @@
     (if (null? lst)
         base-case
         (operator (car lst) (red operator base-case (cdr lst))))))
-
 lst
 "Sum the list"
 (red + 0 lst)
-"Multiply everything in the list"
+"Multiply the list"
 (red * 1 lst)
 "Double every element in a list"
-(red (lambda (x y) (cons (* 2 x) y)) '() lst)
+(red (lambda (carval cdrval) (cons (* 2 carval) cdrval)) '() lst)
 "Length"
 (red (lambda (x y) (+ 1 y)) 0 lst)
+(red (lambda (x y) (+ 1 y)) 0 '(1 2 3 4 5 6 7 8 9 10))
 "Get all even numbers"
 (red (lambda (x y) (if (= 1 (remainder x 2)) y (cons x y))) '() lst)
 "Reverse"
+(red (lambda (x y) (append y (list x))) '() '(a b c d e f g h i))
 (red (lambda (x y) (append y (list x))) '() lst)
 
+(newline)
 "apply, map, filter"
 (apply + lst)
 (apply * lst)
+(map square lst)
 (map (lambda (x) (+ x 3)) lst)
 (map (lambda (x) (/ x 2)) lst)
-(map number? '(1 a 2 b "hello" 'world' + 4 red))
-(filter number? '(1 a 2 b "hello" 'world' + 4 red))
-(apply + (map square (filter number? '(1 a 2 b "hello" 'world' + 4 red))))
+(map number? '(1 a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4)))
+(filter number? '(1 a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4)))
+(filter number? (list 1 'a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4)))
+
+(map square (filter number? (list 1 'a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4))))
 
 (newline)
 "let structures"
@@ -91,19 +95,24 @@ lst
 (define b 5)
 (let
     ((a 10)
-     (b 11))
-  (+ a b))
-
-; be careful
+     (b 11)
+     (c 12))
+  (+ a b c))
 (let
-    ((a 10)
-     (b (+ a 5)))
+    ((x 7)
+     (y 2))
+  (+ x y 10))
+
+; Be carefull!
+(let
+    ((a 5)
+     (b (+ a 1)))  ; bad coding style!!!
   (+ a b))
 
 (define double-pi-list
   (lambda (lst)
     (let
-        ((double (lambda (x) (* x 2)))
+        ((double (lambda (x) (* 2 x)))
          (pi 3.14159))
       (if (null? lst)
           '()
@@ -115,12 +124,9 @@ lst
     ((a 20)
      (b 10))
   (+ a b))
-
 ((lambda (a b) (+ a b)) 20 10)
 
-"lambda -> let"
 ((lambda (m n) (- m n)) 35 17)
-
 (let
     ((m 35)
      (n 17))
@@ -131,26 +137,46 @@ lst
 (define sum
   (lambda (x y)
     (+ x y)))
-
 (define increment-creator
   (lambda (x)
     (lambda (y)
-      (sum x y))))
+      (+ x y))))
+(increment-creator 1)
+((increment-creator 1) 3)
+
 (define increment
   (lambda (n)
     ((increment-creator 1) n)))
+(increment 7)
+
 (define add10
   (lambda (n)
     ((increment-creator 10) n)))
+(add10 7)
+(map add10 '(7 4 38 89 103))
 
-(increment 42)
-(add10 42)
-
+; f(x)=ax^2 + bx + c
 (define f
   (lambda (a b c)
     (lambda (x)
       (+ (* a x x) (* b x) c))))
-((f 1 1 1) 3)
-((f 1 1 1) -2)
-((f 1 2 3) 7)
 
+"f(x)=x^2 + x + 1"
+(f 1 1 1)
+((f 1 1 1) 3)
+"f(x)=2x^2 + 3x - 5"
+((f 2 3 -5) 3)
+((f 2 3 -5) -1)
+((f 2 3 -5) 10)
+
+(newline)
+"Exercise questions"
+(define parens-depth
+  (lambda (lst)
+    (cond
+      ((not (list? lst)) 0)
+      ((null? lst) 1)
+      (else (max (+ 1 (parens-depth (car lst))) (parens-depth (cdr lst)))))))
+(parens-depth '(1 2 (3 4) (5 (6 (7 8) 9))))
+(parens-depth 'deep)
+(parens-depth '(1 2 3))
